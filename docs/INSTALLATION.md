4643ef8e91db893c8e319262faae49a03fb58c2b
Spiderjockey2 <benjamin.forey11gmail.com>
2021-05-13 00:23:15 +0100

    updated config.example.js + installation

---
+++ docs/INSTALLATION.md
@@ -0,0 +1,1 @@
+fill in config file
--- src/config.example.js
+++ src/config.example.js
@@ -1,7 +1,16 @@
 const config = {
-	token: 'DiscordBotToken',
-	MongoDBURl: 'MongoDBURl',
+	token: 'YourBotToken',
+	// URL to mongodb
+	MongoDBURl: 'MONGO DB URL',
+	// true or false
 	debug: true,
+	// https://developer.twitter.com/en/portal/dashboard
+	twitter: {
+		consumer_key:	'',
+		consumer_secret:	'',
+		access_token_key:	'',
+		access_token_secret: '',
+	},
 };
 
 module.exports = config;
