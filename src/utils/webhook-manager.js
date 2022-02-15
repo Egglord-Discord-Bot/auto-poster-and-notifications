9b10fb3536f6034041201ee3137a540d4b169f3a
Spiderjockey02 <benjamin.forey11@gmail.com>
2022-02-15 00:38:55 +0000

    Add updated webhook manager

--- src/utils/webhook-manager.js
+++ src/utils/webhook-manager.js
@@ -1,37 +1,73 @@
-module.exports = async (bot) => {
-	// get list of channel ID's
-	const channelIDs = Array.from(bot.embedCollection.keys());
+const { Collection } = require('discord.js');
 
-	// loop through each channel ID sending their embeds
-	for (let i = 0; i < channelIDs.length; i++) {
-		try {
-			const webhooks = await bot.channels.fetch(channelIDs[i]).then(c => c.fetchWebhooks());
-			let webhook = webhooks.find(wh => wh.name == bot.user.username);
+class WebhookManager {
+	constructor(client) {
+		/**
+      * The Discord Client
+      * @type {Discord.Client}
+    */
+		this.client = client;
+		/**
+			* A collection of messages to send to channels via webhook
+			* @type {Discord.Collection}
+		*/
+		this.messages = new Collection();
+	}
 
-			// create webhook if it doesn't exist
-			if (!webhook) webhook = await bot.channels.fetch(channelIDs[i]).then(c => c.createWebhook(bot.user.username));
+	async init() {
+		// get list of channel ID's
+		const channelIDs = Array.from(this.messages.keys());
 
-			// send the embeds
-			const repeats = Math.ceil(bot.embedCollection.get(channelIDs[i]).length / 10);
-			for (let j = 0; j < repeats; j++) {
-				// Get the embeds
-				const embeds = bot.embedCollection.get(channelIDs[i])?.slice(j * 10, (j * 10) + 10);
-				if (!embeds) return;
+		// loop through each channel ID sending their embeds
+		for (const ID of channelIDs) {
+			try {
+				const channel = await this.client.channels.fetch(ID);
+				const webhooks = channel.then(c => c.fetchWebhooks());
+				let webhook = webhooks.find(wh => wh.name == this.client.user.username);
 
-				await webhook.send('', {
-					username: bot.user.name,
-					avatarURL: bot.user.displayAvatarURL({ format: 'png', size: 1024 }),
-					// make sure only 10 embeds are sent
-					embeds: embeds,
-				});
+				// create webhook if it doesn't exist
+				if (!webhook) webhook = channel.then(c => c.createWebhook(this.client.user.username));
+
+				// send the embeds
+				const repeats = Math.ceil(this.messages.get(ID).length / 10);
+				for (let j = 0; j < repeats; j++) {
+					// Get the embeds
+					const embeds = this.messages.get(ID)?.slice(j * 10, (j * 10) + 10);
+					if (!embeds) return;
+
+					await webhook.send({
+						username: this.client.user.name,
+						avatarURL: this.client.user.displayAvatarURL({ format: 'png', size: 1024 }),
+						// make sure only 10 embeds are sent
+						embeds: embeds,
+					});
+				}
+
+				// delete from collection once sent
+				this.messages.delete(ID);
+			} catch (err) {
+				// It was likely they didn't have permission to create/send the webhook
+				this.client.logger.error(err.message);
+				this.messages.delete(ID);
 			}
+		}
 
-			// delete from collection once sent
-			bot.embedCollection.delete(channelIDs[i]);
-		} catch (err) {
-			// It was likely they didn't have permission to create/send the webhook
-			bot.logger.error(err.message);
-			bot.embedCollection.delete(channelIDs[i]);
+	}
+
+	/**
+	 * Function for adding messages to send to the webhook manager
+	 * @param {channelID} String The instantiating client
+	 * @param {embed} Discord.MessageEmbed The message that ran the command
+	 * @readonly
+	*/
+	addValues(channelID, embed) {
+		// collect embeds
+		if (!this.messages.has(channelID)) {
+			this.messages.set(channelID, [embed]);
+		} else {
+			this.messages.set(channelID, [...this.messages.get(channelID), embed]);
 		}
 	}
-};
+}
+
+module.exports = WebhookManager;
